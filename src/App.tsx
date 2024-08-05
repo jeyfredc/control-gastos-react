f0f8cbdd945522231b9dd416add99f4bed587720
Adm-JeyfredC <Adm-JeyfredC@hdi.com.co>
2024-08-05 16:13:34 -0500

    Add reducer and context app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-
+import { useContext } from "react";
+import BudgetForm from "./components/BudgetForm";
+import { BudgetContext } from "./context/BudgetContext";
 
 function App() {
 
+  const context = useContext(BudgetContext)
   return (
     <>
-    <h1>hola mundo</h1>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          {" "}
+          Planificador de Gastos
+        </h1>
+      </header>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        <BudgetForm/>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
