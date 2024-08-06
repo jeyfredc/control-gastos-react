fa8e350fd19b4f1992187aa2b0ab76bffc22dc76
Adm-JeyfredC <Adm-JeyfredC@hdi.com.co>
2024-08-06 17:03:46 -0500

    Add state reducer, swipe, actions, add expense, delete expense, update expense, control modal

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,39 +1,147 @@
 import { categories } from "../data/categories";
+import DatePicker from "react-date-picker";
+import 'react-date-picker/dist/DatePicker.css'
+import 'react-calendar/dist/Calendar.css'
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { DraftExpense, Value } from "../types";
+import ErrorMessage from "./ErrorMessage";
+import { useBudget } from "../hooks/useBudget";
+import { initialState } from '../reducers/budget-reducer';
 
 
 export default function ExpenseForm() {
+
+
+  const initialStateExpense:DraftExpense = {
+    amount:0,
+    expenseName: '',
+    category: '',
+    date: new Date()
+  }
+
+  const [expense, setExpense]= useState<DraftExpense>(initialStateExpense)
+
+  const [error, setError]=useState<string>('')
+
+
+  const {dispatch, state}=useBudget()
+
+
+  useEffect(() => {
+    if(state.editingId){
+      const editingExpense = state.expenses.filter(currentExpense => currentExpense.id === state.editingId)[0]
+      setExpense(editingExpense)
+    }
+  }, [state.editingId])
+  
+
+  const handleChange = (e:ChangeEvent<HTMLInputElement>| ChangeEvent<HTMLSelectElement>)=> {
+    const {name, value}= e.target
+    const isAmountField = ['amount'].includes(name)
+    setExpense({
+      ...expense,
+      [name]: isAmountField ?Number(value): value
+    })
+    
+  }
+
+  const handleChangeDate = (value: Value)=>{
+    setExpense({
+      ...expense,
+      date: value
+    })
+    
+  }
+
+  const handleSubmit = (e:FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+
+    //Validar
+    if(Object.values(expense).includes('')){
+      setError('Todos los campos son obligatorios')
+      return
+    }
+    //Agregar o actualizar gasto
+    if(state.editingId){
+      dispatch({type:'update-expense', payload:{ expense: {id:state.editingId, ...expense}}})
+    }else{
+      dispatch({type:'add-expense', payload:{expense: expense}})
+    }
+    
+    
+    //Reiniciar el state
+    setExpense(initialStateExpense)
+  }
+
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
       <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
         Nuevo Gasto
       </legend>
-      <div className="flex flex-col gap-2">
-        <label htmlFor="expenseName" className="text-xl">Nombre Gasto:</label>
-        <input type="text" id="expenseName" placeholder="Añade el nombre del gasto" className="bg-slate-100 p-2" name="expenseName"/>
 
+      {error && <ErrorMessage>{error} </ErrorMessage>}
+      <div className="flex flex-col gap-2">
+        <label htmlFor="expenseName" className="text-xl">
+          Nombre Gasto:
+        </label>
+        <input
+          type="text"
+          id="expenseName"
+          placeholder="Añade el nombre del gasto"
+          className="bg-slate-100 p-2"
+          name="expenseName"
+          value={expense.expenseName}
+          onChange={handleChange}
+        />
       </div>
       <div className="flex flex-col gap-2">
-        <label htmlFor="amount" className="text-xl">Cantidad:</label>
-        <input type="number" id="amount" placeholder="Añade la cantidad del gasto: ej. 300" className="bg-slate-100 p-2" name="amount"/>
-
+        <label htmlFor="amount" className="text-xl">
+          Cantidad:
+        </label>
+        <input
+          type="number"
+          id="amount"
+          placeholder="Añade la cantidad del gasto: ej. 300"
+          className="bg-slate-100 p-2"
+          name="amount"
+          value={expense.amount}
+          onChange={handleChange}
+        />
       </div>
 
       <div className="flex flex-col gap-2">
-        <label htmlFor="category" className="text-xl">Categoria:</label>
-        <select  id="category" className="bg-slate-100 p-2" 
-        name="category">
-            <option value="">-- Seleccione --</option>
-            {
-                categories.map((category)=>{
-                    return(
-                        <option key={category.id} value={category.name}>{category.name}</option>
-                    )
-                })
-            }
-            </select>
-
+        <label htmlFor="category" className="text-xl">
+          Categoria:
+        </label>
+        <select id="category" className="bg-slate-100 p-2" name="category" value={expense.category}
+        onChange={handleChange}
+        >
+          <option value="">-- Seleccione --</option>
+          {categories.map((category) => {
+            return (
+              <option key={category.id} value={category.name}>
+                {category.name}
+              </option>
+            );
+          })}
+        </select>
       </div>
-      <input type="submit" className="bg-blue-600  cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg" value="Registrar Gasto"/>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="date" className="text-xl">
+          Fecha Gasto:
+        </label>
+          <DatePicker
+          className="bg-slate-100 p-2 border-0"
+          value={expense.date}
+          onChange={handleChangeDate}
+          />
+      </div>
+      <input
+        type="submit"
+        className="bg-blue-600  cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+        value="Registrar Gasto"
+      />
     </form>
-  )
+  );
 }
